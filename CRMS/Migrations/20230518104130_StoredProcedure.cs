b9980f13ae9134adf3d91124fb07652204896cbc
iamSigiel <charl.legista@gmail.com>
2023-05-18 18:43:31 +0800

    Added StroredProcedure

---
+++ CRMS/Migrations/20230518104130_StoredProcedure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRMS.Migrations
+{
+    public partial class StoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder) 
+        { 
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
